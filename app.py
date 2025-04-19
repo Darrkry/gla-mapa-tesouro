4cc8ba36c664a3053c90c42180d1ccda3dac5c9a
Darrkry <joaogabrielmdo@gmail.com>
2025-04-18 23:15:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,8 @@ def upload():
     file.save(filepath)
     return f'Arquivo {filename} recebido com sucesso!'
     
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
